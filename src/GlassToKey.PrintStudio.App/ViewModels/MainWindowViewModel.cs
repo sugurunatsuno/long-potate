66622878369d5dfb60994e48b9a9749463114150
sugurunatsuno <22556230+sugurunatsuno@users.noreply.github.com>
2026-09-14 08:59:59 +0900

    Add main window view model

---
+++ src/GlassToKey.PrintStudio.App/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+namespace GlassToKey.PrintStudio.App.ViewModels;
+
+public sealed class MainWindowViewModel
+{
+    public string Title => "GlassToKey Print Studio";
+    public string Status => "Specification / skeleton";
+}
