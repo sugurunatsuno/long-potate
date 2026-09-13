5dae561d456f60b74810e9add97ee7e3464835ed
sugurunatsuno <22556230+sugurunatsuno@users.noreply.github.com>
2026-09-14 08:59:51 +0900

    Add application entry point

---
+++ src/GlassToKey.PrintStudio.App/Program.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+
+namespace GlassToKey.PrintStudio.App;
+
+internal static class Program
+{
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+
+    public static AppBuilder BuildAvaloniaApp() =>
+        AppBuilder.Configure<App>()
+            .UsePlatformDetect();
+}
